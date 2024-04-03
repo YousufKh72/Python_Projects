a59cdcba2bbd048e8c2be70f8b6df363756627d9
Yousuf Khandaker <kenichishirohama72@gmail.com>
2024-04-03 00:04:00 -0400

    Increased readibility of the code.

--- Turtle Trek/main.py
+++ Turtle Trek/main.py
@@ -4,10 +4,6 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-screen.setup(width=600, height=600)
-screen.tracer(0)
-screen.listen()
-
 car = CarManager()
 player = Player()
 score = Scoreboard()
--- Turtle Trek/player.py
+++ Turtle Trek/player.py
@@ -1,28 +1,32 @@
 from turtle import Turtle
 
-
+# Constants for player's starting position, move distance, and finish line Y-coordinate
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 20
 FINISH_LINE_Y = 280
 
-
 class Player(Turtle):
+    """Represents the player's turtle in the game."""
     
     def __init__(self):
+        """Initialize the player turtle with default settings."""
         super().__init__()
-        self.shape("turtle")
-        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
-        self.penup()
-        self.setheading(90)
-        self.goto(STARTING_POSITION)
-        self.color("black")
-        self.speed("fastest")
+        self.shape("turtle")  # Set turtle shape for the player
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)  # Make the turtle smaller
+        self.penup()  # Ensure the turtle doesn't draw lines
+        self.setheading(90)  # Point the turtle upwards
+        self.goto(STARTING_POSITION)  # Move turtle to the starting position
+        self.color("black")  # Set the turtle's color
+        self.speed("fastest")  # Set the drawing speed to the fastest (affects the move speed)
     
     def moveup(self):
+        """Move the player up by a set distance."""
         self.forward(MOVE_DISTANCE)
     
     def movedown(self):
+        """Move the player down by a set distance."""
         self.backward(MOVE_DISTANCE)
     
     def reset(self):
+        """Reset the player to the starting position."""
         self.goto(STARTING_POSITION)
